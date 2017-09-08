b119860584e28621ade838d6dff004637e8ca4e7
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-09-08 16:06:16 -0500

    Setup Rails api, first_commit (y)

---
+++ config/initializers/active_model_serializers.rb
@@ -0,0 +1,9 @@
+ActiveModel::Serializer.config.adapter = :json
+
+api_mime_types = %W(  
+  application/vnd.api+json
+  text/x-json
+  application/json
+)
+
+Mime::Type.register 'application/vnd.api+json', :json, api_mime_types  
